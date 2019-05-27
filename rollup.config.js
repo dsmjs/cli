e03709d8cf2ee6837d948af067eb797507c23d99
Matt Travi <programmer@travi.org>
2019-05-27 02:14:42 -0500

    build(binary): configured rollup to build the binary

---
+++ rollup.config.js
@@ -0,0 +1,10 @@
+/* eslint import/no-extraneous-dependencies: ['error', {'devDependencies': true}] */
+import autoExternal from 'rollup-plugin-auto-external';
+import json from 'rollup-plugin-json';
+import executable from 'rollup-plugin-executable';
+
+export default {
+  input: 'src/index.js',
+  plugins: [autoExternal(), json(), executable()],
+  output: [{file: 'bin/dsmjs.js', format: 'cjs', sourcemap: true, banner: '#!/usr/bin/env node'}]
+};
