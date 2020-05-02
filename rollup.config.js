5accbd61b7a6181f28a7e35ce750a936fc70a785
Matt Travi <programmer@travi.org>
2020-05-02 17:00:36 -0500

    build(rollup): enabled the node-resolve plugin

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,10 @@
 import autoExternal from 'rollup-plugin-auto-external';
 import json from 'rollup-plugin-json';
 import executable from 'rollup-plugin-executable';
+import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
   input: 'src/index.js',
-  plugins: [autoExternal(), json(), executable()],
+  plugins: [autoExternal(), json(), executable(), nodeResolve({mainFields: ['module']})],
   output: [{file: 'bin/dsmjs.js', format: 'cjs', sourcemap: true, banner: '#!/usr/bin/env node'}]
 };
