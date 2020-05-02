240e1d6a70d262c8a82a6032f15658c29c3cb0d3
Matt Travi <programmer@travi.org>
2020-05-02 16:56:06 -0500

    feat(lift): actually exposed the command

--- src/cli-test.js
+++ src/cli-test.js
@@ -3,6 +3,7 @@ import any from '@travi/any';
 import {assert} from 'chai';
 import * as scaffoldCommand from './commands/scaffold';
 import * as travisTokensCommand from './commands/travis-tokens';
+import * as liftCommand from './commands/lift';
 import cli from './cli';
 
 suite('cli', () => {
@@ -13,12 +14,14 @@ suite('cli', () => {
     const alias = sinon.stub();
     const command = sinon.stub();
     const command2 = sinon.stub();
+    const command3 = sinon.stub();
     const demandCommand = sinon.stub();
     const argv = any.simpleObject();
     scriptName.withArgs('form8ion-utils').returns({usage});
     usage.withArgs('Usage: $0 <cmd> [args]').returns({command});
     command.withArgs(travisTokensCommand).returns({command: command2});
-    command2.withArgs(scaffoldCommand).returns({demandCommand});
+    command2.withArgs(scaffoldCommand).returns({command: command3});
+    command3.withArgs(liftCommand).returns({demandCommand});
     demandCommand.withArgs(1, 'You need at least one command before moving on').returns({help});
     help.withArgs('h').returns({alias});
     alias.withArgs('h', 'help').returns({argv});
