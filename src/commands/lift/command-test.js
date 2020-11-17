7280e819269ce6d06a355f5260437b922e8a9c38
Matt Travi <programmer@travi.org>
2020-11-16 22:24:04 -0600

    feat(travis-to-actions): added lifter to replace Travis CI with GitHub Actions

--- src/commands/lift/command-test.js
+++ src/commands/lift/command-test.js
@@ -1,6 +1,7 @@
 import * as lifter from '@form8ion/lift';
 import {removeGreenkeeper} from '@form8ion/remove-greenkeeper';
 import {scaffold as scaffoldDependabot} from '@form8ion/dependabot-scaffolder';
+import {replace as replaceTravisCiWithGithubActions} from '@form8ion/replace-travis-ci-with-github-actions';
 import {assert} from 'chai';
 import sinon from 'sinon';
 import any from '@travi/any';
@@ -23,7 +24,8 @@ suite('lift command', () => {
       .withArgs({
         scaffolders: {
           Dependabot: scaffoldDependabot,
-          'Remove Greenkeeper': removeGreenkeeper
+          'Remove Greenkeeper': removeGreenkeeper,
+          'Replace Travis CI with GitHub Actions': replaceTravisCiWithGithubActions
         }
       })
       .resolves(liftingResults);
--- src/commands/lift/command.js
+++ src/commands/lift/command.js
@@ -1,12 +1,14 @@
 import {lift} from '@form8ion/lift';
 import {removeGreenkeeper} from '@form8ion/remove-greenkeeper';
 import {scaffold as scaffoldDependabot} from '@form8ion/dependabot-scaffolder';
+import {replace as replaceTravisCiWithGithubActions} from '@form8ion/replace-travis-ci-with-github-actions';
 
 export function handler() {
   return lift({
     scaffolders: {
       Dependabot: scaffoldDependabot,
-      'Remove Greenkeeper': removeGreenkeeper
+      'Remove Greenkeeper': removeGreenkeeper,
+      'Replace Travis CI with GitHub Actions': replaceTravisCiWithGithubActions
     }
   });
 }
